4dbfa31435d4dde00ac431457e35e70089406836
swimmy <xuna9088@hotmail.com>
2023-01-19 14:09:34 -0800

    Add Model class

---
+++ src/main/java/com/springframework/petclinic/model/Pet.java
@@ -0,0 +1,22 @@
+package com.springframework.petclinic.model;
+
+public class Pet {
+    private PetType petType;
+    private Owner owner;
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+}
---
+++ src/main/java/com/springframework/petclinic/model/PetType.java
@@ -0,0 +1,5 @@
+package com.springframework.petclinic.model;
+
+public class PetType {
+    private String name;
+}

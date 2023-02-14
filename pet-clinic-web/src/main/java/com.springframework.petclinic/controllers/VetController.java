42d5540f5c698335084cd20b722bdd8ae6ba94ca
swimmy <xuna9088@hotmail.com>
2023-02-14 11:52:14 -0800

    submit service interface and implementation

--- pet-clinic-web/src/main/java/com.springframework.petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com.springframework.petclinic/controllers/VetController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
     private final VetService vetService;

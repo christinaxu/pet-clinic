b5349d6dd887c5e4bf4341c650b8027dabfbef24
swimmy <xuna9088@hotmail.com>
2023-02-14 11:41:02 -0800

    add dataloader

--- pet-clinic-web/src/main/java/com.springframework.petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com.springframework.petclinic/controllers/OwnerController.java
@@ -1,12 +1,13 @@
 package com.springframework.petclinic.controllers;
 
+import com.springframework.petclinic.services.map.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//@RequestMapping("/owners")
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
-    /*
     private final OwnerService ownerService;
 
     public OwnerController(OwnerService ownerService) {
@@ -21,11 +22,11 @@ public class OwnerController {
 
     }
 
-     */
 
-    @RequestMapping({"","index.html","index","/"})
-    public String petIndex(){
-        System.out.println("test");
-        return "test";
-    }
+
+//    @RequestMapping({"","index.html","index","/"})
+//    public String petIndex(){
+//        System.out.println("test");
+//        return "test";
+//    }
 }
--- pet-clinic-web/src/main/java/com/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springframework/petclinic/bootstrap/DataLoader.java
@@ -1,4 +1,74 @@
 package com.springframework.petclinic.bootstrap;
 
-public class DataLoader {
+
+import com.springframework.petclinic.models.Owner;
+import com.springframework.petclinic.models.Pet;
+import com.springframework.petclinic.models.PetType;
+import com.springframework.petclinic.models.Vet;
+import com.springframework.petclinic.services.map.OwnerService;
+import com.springframework.petclinic.services.map.VetService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+    private final OwnerService ownerService;
+    private final VetService vetService;
+
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+        owner1.setFirstName("Michael");
+        owner1.setLastName("Weston");
+
+        ownerService.save(owner1.getId(), owner1);
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(dog);
+        mikesPet.setOwner(owner1);
+
+        Owner owner2 = new Owner();
+        owner2.setId(2L);
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Glenanne");
+
+        ownerService.save(owner2.getId(),owner2);
+
+        Pet fionasCat = new Pet();
+        fionasCat.setPetType(cat);
+        fionasCat.setOwner(owner2);
+
+
+        Vet vet1 = new Vet();
+        vet1.setFirstName("Sam");
+        vet1.setLastName("Axe");
+
+
+        vetService.save(vet1.getId(),vet1);
+
+        Vet vet2 = new Vet();
+        vet2.setFirstName("Jessie");
+        vet2.setLastName("Porter");
+
+
+        vetService.save(vet2.getId(),vet2);
+
+        System.out.println("Data finish Loading...");
+
+
+
+    }
 }

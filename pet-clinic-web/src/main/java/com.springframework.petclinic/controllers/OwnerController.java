7d362b7eeb457b4d6898aba8b0a6c71eb1d4bb97
swimmy <xuna9088@hotmail.com>
2023-02-14 11:49:51 -0800

    submit service interface and implementation

--- pet-clinic-web/src/main/java/com.springframework.petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com.springframework.petclinic/controllers/OwnerController.java
@@ -1,9 +1,12 @@
 package com.springframework.petclinic.controllers;
 
+import com.springframework.petclinic.models.Owner;
 import com.springframework.petclinic.services.map.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @RequestMapping("/owners")
 @Controller
@@ -18,6 +21,11 @@ public class OwnerController {
     public String listOwners(Model model){
         System.out.println("List All Owners");
         model.addAttribute("owners",ownerService.findAll());
+        Set<Owner> owners = ownerService.findAll();
+        for(Owner o : owners){
+            System.out.println(o.getId() + "   " + o.getFirstName() + "   " + o.getLastName());
+        }
+        model.addAttribute("owners",owners);
         return "owners/index";
 
     }

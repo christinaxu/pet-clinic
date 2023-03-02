73c53567a2f64fc38cd54c4c6baf0ea6559bb37b
swimmy <xuna9088@hotmail.com>
2023-03-02 10:43:19 -0800

    Create PetType Map Service close #6

---
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.springframework.petclinic.services;
+
+import com.springframework.petclinic.models.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long>{
+}
---
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/services/map/PetyTypeService.java
@@ -0,0 +1,6 @@
+package com.springframework.petclinic.services.map;
+
+import com.springframework.petclinic.models.PetType;
+
+public class PetyTypeService extends AbstractServiceMap<PetType, Long>{
+}

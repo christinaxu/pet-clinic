7ac0fd1ef097f31940097bf36c84b6a41ed46b4a
swimmy <xuna9088@hotmail.com>
2023-03-02 10:17:44 -0800

    Create Pet Speciality Entity,Associate to Vet close #4

--- pet-clinic-data/src/main/java/com.springframework.petclinic/models/Vet.java
+++ pet-clinic-data/src/main/java/com.springframework.petclinic/models/Vet.java
@@ -1,5 +1,17 @@
 package com.springframework.petclinic.models;
 
+import java.util.Set;
+
 public class Vet extends Person{
+    private Set<Specialty> specialties;
+
     public Vet(){}
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
